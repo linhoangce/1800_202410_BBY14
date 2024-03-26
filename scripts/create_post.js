559bacbeef233b0579d29e5c2b8ffd1dd9f1ad3c
Linh Hoang <linhoang.ce@gmail.com>
2024-03-25 23:25:11 -0700

    seperated create-post function in a file and configured it to all the pages

---
+++ scripts/create_post.js
@@ -0,0 +1,6 @@
+// Post button
+const postButton = document.getElementById("create-post-btn");
+
+postButton.addEventListener("click", () => {
+    window.location.href = 'createPost.html';
+});
--- scripts/main.js
+++ scripts/main.js
@@ -92,9 +92,3 @@ profileButton.addEventListener("click", () => {
     window.location.href = "profile.html";
 })
 
-// Post button
-const postButton = document.getElementById("create-post-btn");
-
-postButton.addEventListener("click", () => {
-    window.location.href = 'createPost.html';
-});
